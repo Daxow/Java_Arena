c34917adf2308253febeb912fe976c5103f9aadc
Daxow <voxizofficiel@gmail.com>
2026-01-20 10:39:20 +0100

    add monster class

---
+++ monsters/Monster.java
@@ -0,0 +1,46 @@
+package monsters;
+
+public abstract class Monster {
+
+    private String name;
+    private int hp;
+    private int hpMax;
+    private int atk;
+
+    public Monster(String name, int hpMax, int atk) {
+        this.name = name;
+        this.hpMax = hpMax;
+        this.hp = hpMax;
+        this.atk = atk;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getHp() {
+        return hp;
+    }
+
+    public int getHpMax() {
+        return hpMax;
+    }
+
+    public int getAtk() {
+        return atk;
+    }
+
+    public boolean isKO() {
+        return hp <= 0;
+    }
+
+    public void receiveDamage(int damage) {
+        hp -= damage;
+        if (hp < 0) hp = 0;
+    }
+
+    public void heal(int points) {
+        hp += points;
+        if (hp > hpMax) hp = hpMax;
+    }
+}
